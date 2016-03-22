d4015db6f23786e8e90eb31c1b016dd062c57c84
Angelo Ashmore <angeloashmore@me.com>
2016-03-22 13:52:40 -1000

    Refactor mutationHandler

--- src/end/index.js
+++ src/end/index.js
@@ -1,13 +1,13 @@
 import "logger";
 import Logger from "js-logger";
 import { MESSAGE, OBSERVATIONS, QUERIES } from "constants";
-import mutationHandler from "end/mutationHandler";
+import mutationHandlerFactory from "end/mutationHandlerFactory";
 
 function init() {
   // Begin observing mutations on QUERIES.ASSETS.
   const observation = OBSERVATIONS.ASSETS;
   const target = document.querySelector(QUERIES.ASSETS);
-  const handler = mutationHandler(observation);
+  const handler = mutationHandlerFactory(observation);
   const observer = new MutationObserver(ms => ms.forEach(handler));
   const config = {
     attributes: true,
